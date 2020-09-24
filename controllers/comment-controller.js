30f157d3a1619f4b0a9efd1d3f0c7835b0950ea6
jack <biffisbuffm@gmail.com>
2020-09-24 12:12:48 -0500

    replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,32 @@ addComment({ params, body }, res) {
       .catch(err => res.json(err));
   },
 
+  // add reply
+  addReply({ params, body }, res) {
+      Comment.findOneAndUpdate(
+          { _id: params.commentId },
+          { $push: { replies: body } },
+          { new: true }
+      )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this ID'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+  },
+  // delete reply
+  removeReply({ params }, res) {
+      Comment.findOneAndUpdate(
+          { _id: params.commentId },
+          { $pull: { replies: { replyId: params.replyId } } },
+          { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
+    },
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
